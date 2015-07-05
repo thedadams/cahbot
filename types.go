1e294d51205bd0aafa674e38c7ec2d1f19913ce4
Donnie Adams <donald.g.adams@me.com>
2015-07-05 16:07:15 -0700

    Added a few comments

--- types.go
+++ types.go
@@ -4,16 +4,19 @@ import (
 	"tgbotapi"
 )
 
+// A wrapper for tgbotapi. We need this wrapper to add new methods.
 type CAHBot struct {
     *tgbotapi.BotAPI
 }
 
+// Struct that represents an instance of a game.
 type CAHGame struct {
 	ShuffledCards []int
 	Players       map[string]PlayerGameInfo
 	Settings      GameSettings
 }
 
+// Struct that represents a player in a game.
 type PlayerGameInfo struct {
 	Player          tgbotapi.User
 	Points          int
@@ -22,6 +25,7 @@ type PlayerGameInfo struct {
 	IsMysteryPlayer bool
 }
 
+// Settings for game.
 type GameSettings struct {
 	MysteryPlayer             bool
 	TradeInTwoCardsEveryRound bool
