18276bffa44c4709ab3b1a06df11f8fcdcf3af78
Donnie Adams <donald.g.adams@me.com>
2015-07-05 15:09:49 -0700

    Preliminary type setup

--- types.go
+++ types.go
@@ -5,14 +5,22 @@ import (
 )
 
 type CAHGame struct {
-	Bot           tgbotapi.BotAPI
 	ShuffledCards []int
 	Players       map[string]PlayerGameInfo
+	Settings      GameSettings
 }
 
 type PlayerGameInfo struct {
-	Player     tgbotapi.User
-	Points     int
-	Cards      []int
-	IsCardTzar bool
+	Player          tgbotapi.User
+	Points          int
+	Cards           []int
+	IsCardTzar      bool
+	IsMysteryPlayer bool
+}
+
+type GameSettings struct {
+	MysteryPlayer             bool
+	TradeInTwoCardsEveryRound bool
+	PickWorstToo              bool
+	NumCardsInHand            int
 }
